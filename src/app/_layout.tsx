d223ffb7f340a21882856329b20f9f0c8220b83d
Gerardo Renovales <2872969+grenovales@users.noreply.github.com>
2025-03-08 21:09:28 -0500

    implement expo-router

---
+++ src/app/_layout.tsx
@@ -0,0 +1,9 @@
+/**
+ * Layout component for the app
+ */
+
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return <Stack />;
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+/**
+ * Home screen
+ */
+
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return <View><Text>Home</Text></View>;
+}
